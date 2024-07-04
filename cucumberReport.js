b1cdb98793c4c756718d4f5c23b47e1710d33f48
CLV\hoang.nx <hoang.nx@cyberlogitec.com>
2024-07-04 14:23:05 +0700

    Fix: report should have duration, browser, ...

--- cucumberReport.js
+++ cucumberReport.js
@@ -1,16 +1,57 @@
-const report = require("multiple-cucumber-html-reporter");
+const cypress = require('cypress');
+const report = require('multiple-cucumber-html-reporter');
+const fs = require('fs');
 
-report.generate({
-  jsonDir: "./cypress/reports/cucumber-json/",
-  reportPath: "./cypress/reports/cucumber-html/",
-  metadata: {
-    browser: {
-      name: "chrome",
-      version: "126.0.6478.127",
+
+const mapOs = (os) => {
+  if (os.startsWith('win')) {
+    return 'windows';
+  } else if (os.startsWith('osx')) {
+    return 'osx';
+  } else if (os.startsWith('linux')) {
+    return 'linux';
+  } else if (os.startsWith('ubuntu')) {
+    return 'ubuntu';
+  } else if (os.startsWith('android')) {
+    return 'android';
+  } else if (os.startsWith('ios')) {
+    return 'ios';
+  }
+};
+
+fs.readFile('cypress/reports/hoang-results.json', function read(err, data) {
+  if (err) {
+    throw err;
+  }
+  var runInfos = JSON.parse(data);
+  report.generate({
+    jsonDir: './cypress/reports/cucumber-json/',
+    reportPath: './cypress/reports/cucumber-html/',
+    displayDuration: true,
+    // durationInMS: true,
+    metadata: {
+      browser: {
+        name: runInfos.browserName,
+        version: runInfos.browserVersion,
+      },
+      device: 'Cypress',
+      platform: {
+        name: mapOs(runInfos.osName),
+      },
     },
-    device: "Local test machine",
-    platform: {
-      name: "windows",
+    customData: {
+      title: 'Run info',
+      data: [
+        { label: 'Project', value: 'project' },
+        {
+          label: 'Execution Start Time',
+          value: new Date(runInfos.startedTestsAt).toLocaleString(),
+        },
+        {
+          label: 'Execution End Time',
+          value: new Date(runInfos.endedTestsAt).toLocaleString(),
+        },
+      ],
     },
-  },
+  });
 });
--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+const fs = require('fs');
 
 export default defineConfig({
   e2e: {
@@ -19,6 +20,12 @@ export default defineConfig({
           plugins: [createEsbuildPlugin(config)],
         })
       );
+      on('after:run', (results) => {
+        if (results) {
+          fs.mkdirSync("cypress/reports", { recursive: true });
+          fs.writeFile("cypress/reports/hoang-results.json", JSON.stringify(results));
+        }
+      })
 
       // Make sure to return the config object as it might have been modified by the plugin.
       return config;
